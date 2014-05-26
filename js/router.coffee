2bdbb8fc56f3fa87358c91e6915e24e14d457052
sol0mka <legomushroom@gmail.com>
2014-05-26 21:59:10 +0300

    add not shared posts

--- js/router.coffee
+++ js/router.coffee
@@ -3,9 +3,23 @@
 
 Project.Router.map ->
   @resource 'posts', path: '/', ->
+    @route 'active'
+    @route 'shared'
 
-# Project.PostsRoute = Ember.Route.extend
-#   model:-> @store.find 'post'
+Project.PostsActiveRoute = Ember.Route.extend
+  model:->
+    @store.filter 'post', (post)-> !post.get 'isShared'
+  renderTemplate:(controller)->
+    @render 'posts/index', controller: controller
+
+Project.PostsSharedRoute = Ember.Route.extend
+  model:->
+    @store.filter 'post', (post)-> post.get 'isShared'
+  renderTemplate:(controller)->
+    @render 'posts/index', controller: controller
+
+Project.PostsRoute = Ember.Route.extend
+  model:-> @store.find 'post'
 
 Project.PostsIndexRoute = Ember.Route.extend
-  model: -> @modelFor 'posts'
+  model:-> @modelFor 'posts'
--- js/router.js
+++ js/router.js
@@ -2,7 +2,42 @@
   Project.Router.map(function() {
     return this.resource('posts', {
       path: '/'
-    }, function() {});
+    }, function() {
+      this.route('active');
+      return this.route('shared');
+    });
+  });
+
+  Project.PostsActiveRoute = Ember.Route.extend({
+    model: function() {
+      return this.store.filter('post', function(post) {
+        return !post.get('isShared');
+      });
+    },
+    renderTemplate: function(controller) {
+      return this.render('posts/index', {
+        controller: controller
+      });
+    }
+  });
+
+  Project.PostsSharedRoute = Ember.Route.extend({
+    model: function() {
+      return this.store.filter('post', function(post) {
+        return post.get('isShared');
+      });
+    },
+    renderTemplate: function(controller) {
+      return this.render('posts/index', {
+        controller: controller
+      });
+    }
+  });
+
+  Project.PostsRoute = Ember.Route.extend({
+    model: function() {
+      return this.store.find('post');
+    }
   });
 
   Project.PostsIndexRoute = Ember.Route.extend({
