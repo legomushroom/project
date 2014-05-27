8a5517f0518be8f23832e707d8b57a2110850ee8
sol0mka <legomushroom@gmail.com>
2014-05-27 19:27:27 +0300

    add index/plugin way

--- css/assets/root-selector.js
+++ css/assets/root-selector.js
@@ -1,12 +1,23 @@
+utils = require('../../node_modules/gulp-stylus/node_modules/stylus/lib/utils')
+
 var plugin = function(){
     return function(style){
         style.define('root-selector', function() {
           return this.selectorStack.slice(0,1).toString();
         });
+
+        style.define('rewind-selector', function(i) {
+          var stack = this.selectorStack;
+          len = stack.length
+          i = parseInt(i, 10);
+          if (i == null || !i) i = 1;
+          i = i < 0 ? -i : i;
+          i = i >= len ? len - 1 : i;
+          stack = stack.slice(0,len-i);
+          return utils.compileSelectors(stack);
+        });
   
-    };	
+    };  
 };
 module.exports = plugin;
-
-
 
