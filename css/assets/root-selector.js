baa18960c09f27a98c969e697b49e3b05cf91532
sol0mka <legomushroom@gmail.com>
2014-06-05 23:13:43 +0300

    add the compile selectors fun

--- css/assets/root-selector.js
+++ css/assets/root-selector.js
@@ -1,4 +1,56 @@
-utils = require('../../node_modules/gulp-stylus/node_modules/stylus/lib/utils')
+
+compileSelectors = function(arr, leaveHidden){
+  var self = this
+    , selectors = []
+    , buf = []
+    , hiddenSelectorRegexp = /^\s*\/?\$/;
+
+  function interpolateParent(selector, buf) {
+    var str = selector.val.replace(/^\//g, '').trim();
+    if (buf.length) {
+      for (var i = 0, len = buf.length; i < len; ++i) {
+        if (~buf[i].indexOf('&') || '/' === buf[i].charAt(0)) {
+          str = buf[i].replace(/&/g, str).replace(/^\//g, '').trim();
+        } else {
+          str += ' ' + buf[i].trim();
+        }
+      }
+    }
+    return str.trim();
+  }
+
+  function compile(arr, i) {
+    if (i) {
+      arr[i].forEach(function(selector){
+        if (!leaveHidden && selector.val.match(hiddenSelectorRegexp)) return;
+        if (selector.inherits) {
+          buf.unshift(selector.val);
+          compile(arr, i - 1);
+          buf.shift();
+        } else {
+          selectors.push(interpolateParent(selector, buf));
+        }
+      });
+    } else {
+      arr[0].forEach(function(selector){
+        if (!leaveHidden && selector.val.match(hiddenSelectorRegexp)) return;
+        var str = interpolateParent(selector, buf);
+        if (~str.indexOf('&')) str = str.replace(/&/g, '').trim();
+        if (!str.length) return;
+        selectors.push((self.indent || '') + str.trimRight());
+      });
+    }
+  }
+
+  compile(arr, arr.length - 1);
+
+  // Return the list with unique selectors only
+  return selectors.filter(function(value, index, self){
+    return self.indexOf(value) === index;
+  });
+};
+
+
 
 var plugin = function(){
     return function(style){
@@ -29,12 +81,12 @@ var plugin = function(){
           // get current selector
           stack = stack.slice(0,len-i);
           // return comiled current selector
-          return utils.compileSelectors(stack);
+          return compileSelectors(stack);
         });
 
         style.define('last-selector', function() {
           var len = this.selectorStack.length;
-          var selector = utils.compileSelectors(this.selectorStack);
+          var selector = compileSelectors(this.selectorStack);
           selector = selector.toString().split(' ');
           return selector[selector.length-1];
           // return this.selectorStack.slice(len-1,len).toString();
