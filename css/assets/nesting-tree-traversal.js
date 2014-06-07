2f93318a7157ea694e1062aa1f017d21319961ff
sol0mka <legomushroom@gmail.com>
2014-06-07 23:06:26 +0300

    up function small fix

--- css/assets/nesting-tree-traversal.js
+++ css/assets/nesting-tree-traversal.js
@@ -61,26 +61,13 @@ var plugin = function(){
         style.define('up', function(i) {
           var stack = this.selectorStack,
               len   = stack.length, i;
-          // parse int from i in case of '1a'
-          // string was specified
+          if (!stack.length) return '';
           i = parseInt(i, 10);
-          // if i is undefined/NaN or 0
-          // then set i to 1
-          // i = (!i) ? 1 : i;
-          (!i) && (i = 1);
-
-          // if i < 0 then change sign
-          // takes care of negative i
-          // if somebody will define rewind-selector(-2)
-          // i = i < 0 ? -i : i;
-          (i < 0) && (i = -i);
-          // if i is too big, return root(the last available) selector
-          i = i >= len ? len - 1 : i;
-          // slice selector stack to
-          // get current selector
+          if (!i) (i = 1);
+          if (i < 0) (i = -i);
+          i = (i >= len) ? len - 1 : i;
           stack = stack.slice(0,len-i);
-          // return comiled current selector
-          return compileSelectors(stack);
+          return utils.compileSelectors(stack).join(',');
         });
 
         style.define('last', function() {
